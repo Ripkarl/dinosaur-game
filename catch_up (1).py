3120f86eb7faaa9740ba038325cf00bd1db01dd4
Ripkarl <126473306+Ripkarl@users.noreply.github.com>
2023-08-14 19:03:03 +0700

    Add files via upload

--- catch_up (1).py
+++ catch_up (1).py
@@ -1,12 +1,17 @@
 import pygame
+import random
+import json
 
 pygame.init()
 
-win = pygame.display.set_mode((800, 400))
+win = pygame.display.set_mode((1000, 400))
 clock = pygame.time.Clock()
 
+font = pygame.font.Font (None, 36)
+lose_msg = font.render ('HAHA, YOU LOSE!!!!', False, (255, 255, 255))
+
 background = pygame.image.load('PlayfulWeeklyGuppy-mobile.jpg')
-background = pygame.transform.scale(background, (800, 400))
+background = pygame.transform.scale(background, (1000, 400))
 
 class Dino(pygame.sprite.Sprite):
     def __init__(self, filename, x, y, width, height):
@@ -22,28 +27,33 @@ class Dino(pygame.sprite.Sprite):
 dino = Dino('dino_cool.png', 50, 190, 50, 50)
 
 class Cactus(pygame.sprite.Sprite):
-    def __init__(self, filename, x, y, width, height):
+    def __init__(self, filename, x, y, width, height ):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(pygame.image.load(filename), (width, height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        
+    
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
     def update(self):
+        self.rect.x += -5
 
-        if self.rect.x > 800:
+        if self.rect.x > 1000:
             self.rect.x = 0
         
         if self.rect.x < 0:
-            self.rect.x = 800
+            self.rect.x = 1000 
 
-cactus = Cactus('cactus_cool.png', 700, 190, 50, 50)
-cactus1 = Cactus('cactus_cool.png', 500, 190, 50, 50)
-cactus2 = Cactus('cactus_cool.png', 250, 190, 50, 50)
-cactus_list = [cactus, cactus1, cactus2]
+cactus = pygame.sprite.Group()
+a = Cactus('cactus_cool.png', 700, 190, 70, 50)
+a1 = Cactus('cactus_cool.png', 500, 190, 70, 50)
+a2 = Cactus('cactus_cool.png', 250, 190, 70, 50)
+cactus.add(a1)
+cactus.add(a2)
+cactus.add(a)
+
 
 vy = 0
 run = True    
@@ -55,9 +65,12 @@ while run:
         elif e.type == pygame.KEYDOWN:
             if e.key == pygame.K_SPACE:
                 vy = 15
-#        elif dino.rect.y > 15:
-            
-    
+
+    cactus_collisions = pygame.sprite.spritecollide(dino, cactus , False)
+    for a in cactus_collisions:
+        if a.rect.width > 25:
+            lose_msg.blit( )
+
     dino.rect.y -= vy
     if dino.rect.y < 190:
         vy -= 1
@@ -68,8 +81,9 @@ while run:
 
     win.blit(background, (0, 0))
     dino.draw (win)
-    Cactus.draw (win)
-    for c in cactus_list:
+    cactus.update ()
+    
+    for c in cactus:
         c.draw (win)
     pygame.display.update()
     clock.tick(60)
